6d3b52e1f2eaa6051454873445ff233f3d33e6a6
112frosta <112frosta@proton.me>
2024-07-16 00:11:06 +0200

    Add useHandler to entry

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import { Client } from "discord.js";
+import { useHandlers } from "@/handlers";
 
 const client = new Client({
   intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
 });
 
-client.on("ready", () => {
+client.on("ready", async () => {
+  await useHandlers(client);
   console.log(`Logged in as ${client.user?.tag}`);
 });
 
